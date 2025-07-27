2ea64d88c176b6ebe75c563252e4123722ac2b5c
brayan <marcosbrayan03@gmail.com>
2025-07-27 21:57:25 +0200

    fix(next.config): add supabase domain to image optimization whitelist

--- next.config.js
+++ next.config.js
@@ -34,6 +34,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "i.imgur.com",
       },
+      {
+        protocol: "https",
+        hostname: "duzwyvvyksetejhwrcuo.supabase.co",
+      },
     ],
   },
 };
