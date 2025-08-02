d3fa6f9a850ff8f6a82be748af87b3e34e2d911d
brayan <marcosbrayan03@gmail.com>
2025-08-02 19:25:33 +0200

    style(ChannelList): add dark mode support to channel preview component

--- components/ChannelList/CustomChannelPreview.tsx
+++ components/ChannelList/CustomChannelPreview.tsx
@@ -13,13 +13,13 @@ const CustomChannelPreview = (props: ChannelPreviewUIComponentProps) => {
       }`}
     >
       <button
-        className="w-full flex items-center px-2 py-1 hover:bg-gray-100  rounded-md group"
+        className="w-full flex items-center px-2 py-1 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-md group"
         onClick={() => setActiveChannel(channel)}
       >
-        <span className="text-gray-400 text-xl font-semibold mr-2 group-hover:text-gray-500">
+        <span className="text-gray-400 dark:text-gray-500 text-xl font-semibold mr-2 group-hover:text-gray-500 dark:group-hover:text-gray-400">
           #
         </span>
-        <span className="text-sm font-medium text-black group-hover:text-gray-800">
+        <span className="text-sm font-medium text-black dark:text-gray-200 group-hover:text-gray-800 dark:group-hover:text-white">
           {channel.data?.name || "Channel Preview"}
         </span>
       </button>
