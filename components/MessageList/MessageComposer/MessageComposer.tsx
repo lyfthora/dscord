9000484f00b3c6f6d242764265be1f81a9d68238
brayan <marcosbrayan03@gmail.com>
2025-08-02 19:26:47 +0200

    dark mode

--- components/MessageList/MessageComposer/MessageComposer.tsx
+++ components/MessageList/MessageComposer/MessageComposer.tsx
@@ -14,13 +14,13 @@ export default function MessageComposer(): JSX.Element {
   const { channel } = useChatContext();
   const [message, setMessage] = useState('');
   return (
-    <div className='flex mx-6 my-6 px-4 py-1 bg-composer-gray items-center justify-center space-x-4 rounded-md text-gray-600 relative'>
+    <div className='flex mx-6 my-6 px-4 py-1 bg-composer-gray dark:bg-gray-700 items-center justify-center space-x-4 rounded-md text-gray-600 dark:text-gray-400 relative'>
       <button onClick={() => setPlusMenuOpen((menuOpen) => !menuOpen)}>
-        <PlusCircle className='w-8 h-8 hover:text-gray-800' />
+        <PlusCircle className='w-8 h-8 hover:text-gray-800 dark:hover:text-gray-200' />
       </button>
       {plusMenuOpen && (
         <div className='absolute p-2 z-10 -left-6 bottom-12'>
-          <div className='bg-white p-2 shadow-lg rounded-md w-40 flex flex-col'>
+          <div className='bg-white dark:bg-gray-800 p-2 shadow-lg rounded-md w-40 flex flex-col'>
             {plusItems.map((option) => (
               <button
                 key={option.name}
@@ -34,15 +34,15 @@ export default function MessageComposer(): JSX.Element {
         </div>
       )}
       <input
-        className='border-transparent bg-transparent outline-none text-sm font-semibold m-0 text-gray-normal'
+        className='border-transparent bg-transparent outline-none text-sm font-semibold m-0 text-gray-normal dark:text-gray-200'
         type='text'
         value={message}
         onChange={(e) => setMessage(e.target.value)}
         placeholder='Message #general'
       />
-      <Present className='w-8 h-8 hover:text-gray-800' />
-      <GIF className='w-8 h-8 hover:text-gray-800' />
-      <Emoji className='w-8 h-8 hover:text-gray-800' />
+      <Present className='w-8 h-8 hover:text-gray-800 dark:hover:text-gray-200' />
+      <GIF className='w-8 h-8 hover:text-gray-800 dark:hover:text-gray-200' />
+      <Emoji className='w-8 h-8 hover:text-gray-800 dark:hover:text-gray-200' />
       <SendButton
         sendMessage={() => {
           channel?.sendMessage({ text: message });
