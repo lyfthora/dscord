f768b68cab87b2d27894fa82a6987b06ecd72b8a
brayan <marcosbrayan03@gmail.com>
2025-08-02 19:26:45 +0200

    dark mode

--- components/MessageList/CustomMessage/CustomMessage.tsx
+++ components/MessageList/CustomMessage/CustomMessage.tsx
@@ -15,7 +15,7 @@ export default function CustomMessage(): JSX.Element {
     <div
       onMouseEnter={() => setShowOptions(true)}
       onMouseLeave={() => setShowOptions(false)}
-      className='flex relative space-x-2 p-2 rounded-md transition-colors ease-in-out duration-200 hover:bg-gray-100'
+      className='flex relative space-x-2 p-2 rounded-md transition-colors ease-in-out duration-200 hover:bg-gray-100 dark:hover:bg-gray-800'
     >
       <Image
         className='rounded-full aspect-square object-cover w-10 h-10'
@@ -34,16 +34,16 @@ export default function CustomMessage(): JSX.Element {
           </div>
         )}
         <div className='space-x-2'>
-          <span className='font-semibold text-sm text-black'>
+          <span className='font-semibold text-sm text-black dark:text-white'>
             {message.user?.name}
           </span>
           {message.updated_at && (
-            <span className='text-xs text-gray-600'>
+            <span className='text-xs text-gray-600 dark:text-gray-400'>
               {formatDate(message.updated_at)}
             </span>
           )}
         </div>
-        <p className='text-sm text-gray-700'>{message.text}</p>
+        <p className='text-sm text-gray-700 dark:text-gray-300'>{message.text}</p>
         <ReactionsList />
       </div>
     </div>
