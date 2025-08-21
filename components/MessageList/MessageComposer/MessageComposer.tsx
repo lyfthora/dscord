53632e5d88fb210647323c890abffcef19fb90f4
brayan <marcosbrayan03@gmail.com>
2025-08-21 22:44:52 +0200

    style(MessageComposer): fix duplicate border property and add dark mode border
    Add missing border to plus menu dropdown and remove duplicate border property in composer

--- components/MessageList/MessageComposer/MessageComposer.tsx
+++ components/MessageList/MessageComposer/MessageComposer.tsx
@@ -14,13 +14,13 @@ export default function MessageComposer(): JSX.Element {
   const { channel } = useChatContext();
   const [message, setMessage] = useState("");
   return (
-    <div className="flex mx-6 my-6 px-4 py-1 bg-composer-gray dark:bg-[var(--hover-dark-gray)] border dark:border-[var(--border-color-chat)]  items-center justify-center space-x-4 rounded-md text-gray-600 dark:text-gray-400 relative">
+    <div className="flex mx-6 my-6 px-4 py-1 bg-composer-gray dark:bg-[var(--hover-dark-gray)] border dark:border-[var(--border-color-chat)] border  items-center justify-center space-x-4 rounded-md text-gray-600 dark:text-gray-400 relative">
       <button onClick={() => setPlusMenuOpen((menuOpen) => !menuOpen)}>
         <PlusCircle className="w-8 h-8 hover:text-gray-800 dark:hover:text-gray-200" />
       </button>
       {plusMenuOpen && (
         <div className="absolute p-2 z-10 -left-6 bottom-12">
-          <div className="bg-white dark:bg-[var(--hover-dark-gray)] p-2 shadow-lg rounded-md w-40 flex flex-col">
+          <div className="bg-white dark:bg-[var(--hover-dark-gray)] p-2 shadow-lg border dark:border-[var(--border-color-chat)] rounded-md w-40 flex flex-col">
             {plusItems.map((option) => (
               <button
                 key={option.name}
