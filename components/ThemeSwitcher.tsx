ee29460876801e662f6e175842f21314c8d4d667
brayan <marcosbrayan03@gmail.com>
2025-10-01 18:02:09 +0200

    refactor(ThemeSwitcher): replace custom SVG icons with Image component

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -1,32 +1,7 @@
 "use client";
 
 import { useTheme } from "next-themes";
-
-const SunIcon = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    {...props}
-  >
-    <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.106a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5h2.25a.75.75 0 01.75.75zM17.836 17.836a.75.75 0 00-1.06-1.06l-1.59 1.591a.75.75 0 101.06 1.06l1.59-1.591zM12 18.75a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-1.5a.75.75 0 01.75-.75zM5.106 17.836a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM3 12a.75.75 0 01-.75.75H.75a.75.75 0 010-1.5h2.25A.75.75 0 013 12zM6.106 5.106a.75.75 0 00-1.06 1.06l1.59 1.591a.75.75 0 001.06-1.06l-1.59-1.591z" />
-  </svg>
-);
-
-const MoonIcon = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    {...props}
-  >
-    <path
-      fillRule="evenodd"
-      d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 004.463-.948.75.75 0 01.818.162a9 9 0 01-9.463 9.463A9 9 0 011.718 9.528a9 9 0 019.463-9.463.75.75 0 01-.654.654z"
-      clipRule="evenodd"
-    />
-  </svg>
-);
+import Image from "next/image";
 
 export const ThemeSwitcher = () => {
   const { theme, setTheme } = useTheme();
@@ -37,9 +12,9 @@ export const ThemeSwitcher = () => {
       className="p-2 rounded-full text-gray-500 hover:bg-gray-200 dark:text-gray-400 dark:hover:bg-[var(--hover-dark-gray)] transition-colors"
     >
       {theme === "dark" ? (
-        <SunIcon className="w-5 h-5" />
+        <Image src="/light.svg" alt="Light mode" width={20} height={20} />
       ) : (
-        <MoonIcon className="w-5 h-5" />
+        <Image src="/dark.svg" alt="Dark mode" width={20} height={20} />
       )}
     </button>
   );
